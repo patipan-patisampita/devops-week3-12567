70642a75e09b3dcab1aa4a97eee46b87740bcc48
patipan <patipan.patisampita@gmail.com>
2024-06-04 11:51:56 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,21 @@
+<script setup>
+import { ref, reactive } from 'vue'
+const name = ref()
+const state = reactive({age:25})
+</script>
+
 <template>
   <nav>
     <h3>Home page</h3>
+    <p>Name: {{ name }} Age: {{ state.age }}</p>
+    Name:<input type="text" v-model="name">
+    Age:<input type="text" v-model="state.age">
   </nav>
 </template>
 
 <style scoped>
-  nav{
-    padding: 10px;
-    text-align: center;
-  }
+nav {
+  padding: 10px;
+  text-align: center;
+}
 </style>
