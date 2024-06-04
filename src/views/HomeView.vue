df37c07dbbb77cfdbc7a6506366bbf811b3f854b
patipan <patipan.patisampita@gmail.com>
2024-06-04 12:06:00 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,21 @@
 <script setup>
 import { ref, reactive } from 'vue'
 const name = ref()
-const state = reactive({age:25})
+const state = reactive({ age: null })
+const ceo = ref('Mark')
 </script>
 
 <template>
   <nav>
     <h3>Home page</h3>
+    <span v-if="ceo == 'Jhon'">
+      {{ ceo }}
+    </span>
+
+    <span v-else="ceo == 'Mark'">
+      {{ ceo }}
+    </span>
+
     <p>Name: {{ name }} Age: {{ state.age }}</p>
     Name:<input type="text" v-model="name">
     Age:<input type="text" v-model="state.age">
