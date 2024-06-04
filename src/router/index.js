1becbd53ff114d21c03b5716c79130648bfefbe2
patipan <patipan.patisampita@gmail.com>
2024-06-04 11:22:24 +0700

    update

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../views/ProfileView.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <h3>Home page</h3>
+  </nav>
+</template>
+
+<style scoped>
+  nav{
+    padding: 10px;
+    text-align: center;
+  }
+</style>
